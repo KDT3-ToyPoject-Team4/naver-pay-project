61e2f4863beb9f8244696f9f9c6f45bf2c9d20b1
HongGuma <hongsu9849@gmail.com>
2022-10-31 21:03:08 +0900

    environment setting

---
+++ src/main/java/com/toyprojcet/naver_pay/NaverPayApplication.java
@@ -0,0 +1,13 @@
+package com.toyprojcet.naver_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyprojcet/naver_pay/NaverPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyprojcet.naver_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
