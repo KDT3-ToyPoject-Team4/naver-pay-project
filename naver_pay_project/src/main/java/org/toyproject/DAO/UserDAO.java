2117e01aadb2d506fb967718dd2d449f9b69e332
HongGuma <hongsu9849@gmail.com>
2022-11-05 14:07:59 +0900

    feat: Create a userDAO
    userDAO 생성, DAO 테스트 파일도 생성, crawlingDAO 삭제

--- naver_pay_project/src/main/java/org/toyproject/DAO/UserDAO.java
+++ naver_pay_project/src/main/java/org/toyproject/DAO/UserDAO.java
@@ -1,4 +1,188 @@
 package org.toyproject.DAO;
 
+import org.toyproject.DB.ConnectionPoolMgr;
+import org.toyproject.DTO.UserDTO;
+import org.toyproject.entity.UserEntity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class UserDAO {
+    private ConnectionPoolMgr connectionPoolMgr;
+    private Connection conn = null;
+    private PreparedStatement pstmt = null;
+    private ResultSet rs = null;
+
+    //유저의 모든 정보 삽입
+    private static final String USER_INSERT_ALL = "INSERT INTO user_info(user_id, user_pw, user_name, user_ph, user_addr) VALUES(?, ?, ?, ?, ?)";
+    //유저 필수 정보만 삽입
+    private static final String USER_INSERT = "INSERT INTO user_info(user_id, user_pw, user_name) VALUES(?,?,?)";
+    //전체 데이터 조회 query (디버깅용)
+    private static final String USER_SELECT_ALL = "SELECT * FROM user_info";
+    //특정 유저 조회 (로그인 및 회원 가입 용)
+    private static final String USER_SELECT = "SELECT * FROM user_info WHERE user_id = ? AND user_pw = ?";
+    //유저 이름 수정
+    private static final String USER_UPDATE_NAME = "UPDATE user_info SET user_name = ? WHERE user_serial_num = ?";
+    //유저 주소 수정
+    private static final String USER_UPDATE_ADDRESS = "UPDATE user_info SET user_addr = ? WHERE user_serial_num = ?";
+    //유저 폰 번호 수정
+    private static final String USER_UPDATE_PHONENUMBER = "UPDATE user_info SET user_ph = ? WHERE user_serial_num = ?";
+
+    public UserDAO(){
+        if(connectionPoolMgr == null){
+            connectionPoolMgr = ConnectionPoolMgr.getInstance();
+        }
+    }
+
+    /**
+     * 디버깅용 전체 리스트 조회 하기
+     * @return user 테이블에 있는 모든 값
+     */
+    public List<UserDTO> searchUserALL(){
+        List<UserDTO> userList = new LinkedList<>();
+        try{
+            conn = connectionPoolMgr.getConnection();
+            pstmt = conn.prepareStatement(USER_SELECT_ALL);
+            rs = pstmt.executeQuery();
+            while(rs.next()){
+                Long uSerial = Long.parseLong(rs.getString("user_serial_num"));
+                String uId = rs.getString("user_id");
+                String uPw = rs.getString("user_pw");
+                String uName = rs.getString("user_name");
+                String uPh = rs.getString("user_ph");
+                String uAddr = rs.getString("user_addr");
+                int uPoint = Integer.parseInt(rs.getString("user_point"));
+
+                userList.add(new UserDTO(uSerial,uId,uPw,uName,uPh,uAddr,uPoint));
+            }
+            conn.commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            connectionPoolMgr.freeConnection(conn, pstmt, rs);
+        }
+
+        return userList;
+    }
+
+    /**
+     * 아이디와 비밀번호 입력 받아서 유저 정보 찾기
+     * @param userId : 사용자에게 입력 받은 아이디
+     * @param userPw : 사용자에게 입력 받은 비밀 번호
+     * @return UserDTO 객체 반환, 없으면 null
+     */
+    public UserDTO searchUser(String userId, String userPw){
+        UserDTO userList = null;
+        try{
+            conn = connectionPoolMgr.getConnection();
+            pstmt = conn.prepareStatement(USER_SELECT);
+            pstmt.setString(1,userId);
+            pstmt.setString(2,userPw);
+
+            rs = pstmt.executeQuery();
+            while(rs.next()){
+                Long uSerial = Long.parseLong(rs.getString("user_serial_num"));
+                String uId = rs.getString("user_id");
+                String uPw = rs.getString("user_pw");
+                String uName = rs.getString("user_name");
+                String uPh = rs.getString("user_ph");
+                String uAddr = rs.getString("user_addr");
+                int uPoint = Integer.parseInt(rs.getString("user_point"));
+
+                userList = new UserDTO(uSerial,uId,uPw,uName,uPh,uAddr,uPoint);
+            }
+            conn.commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            connectionPoolMgr.freeConnection(conn, pstmt, rs);
+        }
+
+        return userList;
+    }
+
+    /**
+     * 필수 사용자 정보만 저장
+     * @param userDTO : user DTO 객체
+     * @return DB에 삽입한 데이터 개수 반환 ( 1: 정상, 그 외 나머지 : error)
+     */
+    public int insertUser(UserDTO userDTO){
+        int result = 0;
+        try{
+            conn = connectionPoolMgr.getConnection();
+            pstmt = conn.prepareStatement(USER_INSERT);
+            pstmt.setString(1,userDTO.getUserId());
+            pstmt.setString(2,userDTO.getUserPassword());
+            pstmt.setString(3,userDTO.getUserName());
+            result = pstmt.executeUpdate();
+            conn.commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            connectionPoolMgr.freeConnection(conn,pstmt);
+        }
+        return result;
+    }
+
+    /**
+     * 필수 정보 외의 다른 모든 정보 입력시
+     * @return DB에 삽입한 데이터 개수 반환 ( 1: 정상, 그 외 나머지 : error)
+     */
+    public int insertAllUserInfo(UserDTO userDTO){
+        int result = 0;
+        try{
+            conn = connectionPoolMgr.getConnection();
+            pstmt = conn.prepareStatement(USER_INSERT_ALL);
+            pstmt.setString(1,userDTO.getUserId());
+            pstmt.setString(2,userDTO.getUserPassword());
+            pstmt.setString(3,userDTO.getUserName());
+            pstmt.setString(4,userDTO.getUserPhoneNumber());
+            pstmt.setString(5,userDTO.getUserAddress());
+            result = pstmt.executeUpdate();
+            conn.commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            connectionPoolMgr.freeConnection(conn,pstmt);
+        }
+        return result;
+    }
+
+    //원래 필수 정보 외의 정보(폰번호,주소) 둘중에 하나만 받는 경우도 넣어야 하는데 시간 관계상 생략함
+
+    /**
+     * 사용자 이름 변경
+     * @param userDTO : 사용자 객체
+     * @return 수정 완료한 데이터 개수 ( 1 : 정상, 그 외 나머지 : error)
+     */
+    public int updateUserName(UserDTO userDTO){
+        int result = 0;
+        try{
+            conn = connectionPoolMgr.getConnection();
+            pstmt = conn.prepareStatement(USER_UPDATE_NAME);
+            pstmt.setString(1,userDTO.getUserName());
+            pstmt.setString(2,String.valueOf(userDTO.getUserSerialNum()));
+            result = pstmt.executeUpdate();
+            conn.commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            connectionPoolMgr.freeConnection(conn,pstmt);
+        }
+        return result;
+    }
 }
