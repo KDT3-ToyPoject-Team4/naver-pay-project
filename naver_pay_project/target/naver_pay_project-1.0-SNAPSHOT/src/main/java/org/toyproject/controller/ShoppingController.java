8811dca2471922f1e4b0464d903bde8520956380
WonJoonChoi <724thomas@gmail.com>
2022-11-08 22:44:02 +0900

    PW

---
+++ naver_pay_project/target/naver_pay_project-1.0-SNAPSHOT/src/main/java/org/toyproject/controller/ShoppingController.java
@@ -0,0 +1,15 @@
+package org.toyproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ShoppingController {
+    @RequestMapping(value = "/shopping/getShoppingList", method = RequestMethod.GET)
+    public String Shopping(Model model){
+        model.addAttribute("shoppingList","날짜 입력 전 화면입니다.");
+        return "Shopping";
+    }
+}
