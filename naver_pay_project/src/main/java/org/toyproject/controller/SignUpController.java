7cf7b574c63591c7e75a841aea20d55c9346acec
lala9663 <lala96632040@gmail.com>
2022-11-09 01:12:10 +0900

    feat: 회원가입 폼

---
+++ naver_pay_project/src/main/java/org/toyproject/controller/SignUpController.java
@@ -0,0 +1,38 @@
+package org.toyproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SignUpController {
+    @RequestMapping(value = "/SignUp", method = RequestMethod.GET)
+    public String signup(){
+        return "SignUp";      // WEB-INF/views/RegisterForm.jsp
+    }
+
+
+    @RequestMapping("/sign/success")
+    public String success(){
+
+        return "index";
+    }
+
+
+
+//    @PostMapping("/register/save")
+//    public String save(User user, Model m) throws Exception{
+//        if (!isValid(user)){
+//            String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
+//
+//            m.addAttribute("msg", msg);
+//            return "redirect:/register/add";
+//        }
+//        return "registerInfo";
+//    }
+//
+//    private boolean isValid(User user) {
+//        return true;
+//    }
+
+}
